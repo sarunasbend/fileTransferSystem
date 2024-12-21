0eeabade18b0172fa77234e682ea3017317a8ec7
sarunasbend <s.b.seriouscontact@gmail.com>
2024-12-21 20:52:33 +0000

    Added Fernet Symmetrical Encryption to File Transmission over TCP.

--- Client.py
+++ Client.py
@@ -1,25 +1,24 @@
-import threading
 import socket
-
+from cryptography.fernet import Fernet
 MAX_RECEIVE = 1024
 
 class Client:
-    def __init__(self):
+    def __init__(self, key=None):
         try:
-
+            self.cipher_suite = Fernet(key)
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client_socket.connect(('localhost', 12345))
 
             file_request = "basic.txt"
-            self.client_socket.send(file_request.encode())
-            try:
+            self.client_socket.send(self.cipher_suite.encrypt(file_request.encode("utf-8")))
 
-                with open('basic_downloaded.txt', 'wb') as file:
+            try:
+                with open('download.txt', 'wb') as file:
                     while True:
                         data = self.client_socket.recv(MAX_RECEIVE)
                         if not data:
                             break
-                        file.write(data)
+                        file.write(self.cipher_suite.decrypt(data.decode("utf-8")))
             except FileExistsError as e:
                 print(f"{e}")
         except ConnectionRefusedError as e:
@@ -33,4 +32,5 @@ class Client:
             self.client_socket.close()
 
 if __name__ == "__main__":
-    Client()
+    key = input("Enter the encryption key:").encode()
+    Client(key=key)
--- Server.py
+++ Server.py
@@ -1,39 +1,46 @@
 import socket
 import threading
+from cryptography.fernet import Fernet
 
-IP = '0.0.0.0'
+HOST = '0.0.0.0'
 PORT = 12345
 MAX_RECEIVE = 1024
 
 class Server:
     def __init__(self):
+        self.key = Fernet.generate_key()
+        self.cipher_suite = Fernet(self.key)
+
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server_socket.bind((IP, PORT))
+        self.server_socket.bind((HOST, PORT))
 
-        print("Server Listening on port 12345")
+        print("Server Listening on port: 12345")
+        print(f"Server Encrypted Key: {self.key.decode()}")
         self.server_socket.listen(100)
 
         try:
             while True:
                 client_connection, client_address = self.server_socket.accept()
                 print(f"Connected to {client_address}")
-                threading.Thread(target=self.handleRequest, args=(client_connection,)).start()
+                threading.Thread(target=self.handleRequest, args=(client_connection,client_address,)).start()
         except KeyboardInterrupt as e:
             print("Shutting down Server")
         finally:
             self.server_socket.close()
 
-    def handleRequest(self, client_connection):
+    def handleRequest(self, client_connection, client_address):
         try:
-            file_request = client_connection.recv(MAX_RECEIVE)
+            encrypted_file_request = client_connection.recv(MAX_RECEIVE)
+            decrypted_file_request = self.cipher_suite.decrypt(encrypted_file_request).decode("utf-8")
+            print(f"Message from {client_address}: {decrypted_file_request}")
             while True:
                 try:
-                    with open(file_request, 'rb') as file:
+                    with open(decrypted_file_request, 'rb') as file:
                         data = b""
                         for data_chunk in file:
                             data += data_chunk
-                        client_connection.sendall(data)
+                        client_connection.sendall(self.cipher_suite.encrypt(data))
                         break
                 except FileNotFoundError:
                     print(b"File not Found.")
