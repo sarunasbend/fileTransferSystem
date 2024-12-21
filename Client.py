398fc0d010c4032fc38ee943ecb790251714fb15
sarunasbend <s.b.seriouscontact@gmail.com>
2024-12-21 19:47:18 +0000

    Multi-threaded server can handle multiple simultaneous clients.

--- Client.py
+++ Client.py
@@ -3,18 +3,34 @@ import socket
 
 MAX_RECEIVE = 1024
 
+class Client:
+    def __init__(self):
+        try:
+
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect(('localhost', 12345))
+
+            file_request = "basic.txt"
+            self.client_socket.send(file_request.encode())
+            try:
+
+                with open('basic_downloaded.txt', 'wb') as file:
+                    while True:
+                        data = self.client_socket.recv(MAX_RECEIVE)
+                        if not data:
+                            break
+                        file.write(data)
+            except FileExistsError as e:
+                print(f"{e}")
+        except ConnectionRefusedError as e:
+            print(f"{e}")
+        except KeyboardInterrupt as e:
+            print(f"{e}")
+        except Exception as e:
+            print(f"{e}")
+        finally:
+            print("File has been received.")
+            self.client_socket.close()
+
 if __name__ == "__main__":
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 12345))
-
-    file_request = "basic.txt"
-    client_socket.send(file_request.encode())
-
-    with open ('basic_downloaded.txt', 'wb') as file:
-        while True:
-            data = client_socket.recv(MAX_RECEIVE)
-            if not data:
-                break
-            file.write(data)
-    print("File has been received.")
-
+    Client()
--- Server.py
+++ Server.py
@@ -1,26 +1,49 @@
 import socket
 import threading
 
+IP = '0.0.0.0'
+PORT = 12345
 MAX_RECEIVE = 1024
 
-if __name__ == "__main__":
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', 12345))
-    server_socket.listen(5)
+class Server:
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((IP, PORT))
 
-    print("Server Listening on port 12345")
-
-    while True:
-        client_connection, client_address = server_socket.accept()
-        print(f"Connected to {client_address}")
-        file_request = client_connection.recv(MAX_RECEIVE)
+        print("Server Listening on port 12345")
+        self.server_socket.listen(100)
 
         try:
-            with open(file_request, 'rb') as file:
-                for data_chunk in file:
-                    client_connection.sendall(data_chunk)
-        except FileNotFoundError:
-            print(b"File not Found.")
+            while True:
+                client_connection, client_address = self.server_socket.accept()
+                print(f"Connected to {client_address}")
+                threading.Thread(target=self.handleRequest, args=(client_connection,)).start()
+        except KeyboardInterrupt as e:
+            print("Shutting down Server")
+        finally:
+            self.server_socket.close()
 
-        client_connection.close()
+    def handleRequest(self, client_connection):
+        try:
+            file_request = client_connection.recv(MAX_RECEIVE)
+            while True:
+                try:
+                    with open(file_request, 'rb') as file:
+                        data = b""
+                        for data_chunk in file:
+                            data += data_chunk
+                        client_connection.sendall(data)
+                        break
+                except FileNotFoundError:
+                    print(b"File not Found.")
+                    client_connection.sendall("File not Found".encode())
+                    break
+        except Exception as e:
+            print(f"{e}")
+        finally:
+            client_connection.close()
 
+
+if __name__ == "__main__":
+    Server()
