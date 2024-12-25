c546a2f1a8fa10ee990933c0c89fbc8add4f862b
sarunasbend <s.b.seriouscontact@gmail.com>
2024-12-25 22:31:15 +0000

    Working on the flow of the program.

--- Client.py
+++ Client.py
@@ -3,6 +3,42 @@ import ssl
 
 MAX_RECEIVE = 1024
 
+def startup():
+    print("===========================================================================================")
+    print("______ _ _        _____                    __            _____           _")
+    print("|  ___(_) |      |_   _|                  / _|          /  ___|         | |")
+    print("| |_   _| | ___    | |_ __ __ _ _ __  ___| |_ ___ _ __  \ `--. _   _ ___| |_ ___ _ __ ___")
+    print("|  _| | | |/ _ \   | | '__/ _` | '_ \/ __|  _/ _ \ '__|  `--. \ | | / __| __/ _ \ '_ ` _ \"")
+    print("| |   | | |  __/   | | | | (_| | | | \__ \ ||  __/ |    /\__/ / |_| \__ \ ||  __/ | | | | |")
+    print("\_|   |_|_|\___|   \_/_|  \__,_|_| |_|___/_| \___|_|    \____/ \__, |___/\__\___|_| |_| |_|")
+    print("                                                                __/ |                      ")
+    print("                                                               |___/                       ")
+    print("")
+    print("❈ This is a client-server application that runs on localhost, and uses TLS to communicate")
+    print("  over TCP/IP.")
+    print("❈ Here are the features it allows for:")
+    print("     ❶ Upload Files to Server")
+    print("     ❷ Download Files from Server")
+    print("     ❸ Delete Files from Server")
+    print("     ❹ Share Files with other Clients")
+    print("❈ Press Enter to Start using the System")
+    print("")
+    print("===========================================================================================")
+    input()
+
+def prompt():
+    print("Available Commands:")
+    print("     ❶ connect (connects the client to the server)")
+    print("     ❷ disconnect (disconnects the client from the server)")
+    print("     ❸ upload (uploads a file to the Server, specify the name of the file within current")
+    print("       working directory after command)")
+    print("     ❹ download (specifying the file on the server)")
+    print("     ❺ delete (deletes specified file from server)")
+    print("     ❻ see (lists all files within server)")
+    print("     ❼ exit (close the client)")
+    print("     ❽ help (for this prompt again)")
+    print("")
+
 class Client:
     def __init__(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,38 +49,32 @@ class Client:
         self.secure_client_socket = self.context.wrap_socket(self.client_socket, server_hostname='localhost')
         self.secure_client_socket.connect(('localhost', 12345))
 
-    # will send username and password to server to check if they have access
-    # server will send back OKAY message which will then allow it permissions to make requests, with server then allowing it permissions to access the information
-    # username and password will be sent over in plaintext as TLS will have it encrypted
-    def authenticate(self):
-        username = input("Enter Username: ")
-        password = input("Enter Password: ")
+    def closeClient(self):
+        self.secure_client_socket.close()
+        self.client_socket.close()
 
+    def uploadFile(self, filename):
         try:
-            self.secure_client_socket.send((username + " " + password).encode())
-
-            received_message = self.secure_client_socket.recv(MAX_RECEIVE).decode()
-            print(received_message)
-
-            if received_message != "OK":
-                self.secure_client_socket.close()
-                self.client_socket.close()
-
-        except KeyboardInterrupt as e:
+            while True:
+                try:
+                    with open(filename, 'r') as file:
+                        # data = "upload " + filename + " "
+                        data = b""
+                        for data_chunk in data:
+                            data += data_chunk
+                        self.secure_client_socket.sendall(data)
+                        break
+                except FileNotFoundError:
+                    print("File not Found")
+        except Exception as e:
             print(f"{e}")
-            self.secure_client_socket.close()
-            self.client_socket.close()
-
-    def uploadFile(self):
-        pass
 
     def downloadFile(self):
         try:
             file_request = input("Enter Filename: ")
             self.secure_client_socket.sendall(file_request.encode())
-
             try:
-                with open(file_request + "2", 'wb') as file:
+                with open(file_request, 'wb') as file:
                     while True:
                         data = self.secure_client_socket.recv(MAX_RECEIVE)
                         if not data:
@@ -63,7 +93,29 @@ class Client:
             self.secure_client_socket.close()
             self.client_socket.close()
 
+
+
 if __name__ == "__main__":
-    user = Client()
-    user.authenticate()
-    user.downloadFile()
+    startup()
+    prompt()
+    client = None
+    connected = False
+    while True:
+        userInput = input()
+        if userInput == "connect" and connected == False:
+            client = Client()
+            connected = True
+        elif userInput == "disconnect" and connected == True:
+            client.closeClient()
+        elif userInput.split(" ")[0] == "upload":
+            client.uploadFile(userInput.split(" ")[1])
+        elif userInput.split(" ")[0] == "delete":
+            print("deleting")
+        elif userInput == "see":
+            print("seeing")
+        elif userInput == "exit":
+            break
+        elif userInput == "help":
+            prompt()
+        else:
+            print("invalid command")
--- Server.py
+++ Server.py
@@ -8,8 +8,6 @@ MAX_RECEIVE = 1024
 
 class Server:
     def __init__(self):
-        self.users = {"sarunas" : "xd"}
-        self.authenticated_users = {"sarunas" : False}
 
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -27,14 +25,14 @@ class Server:
             while True:
                 client_connection, client_address = self.secure_server_socket.accept()
                 print(f"Connected to {client_address}")
-                threading.Thread(target=self.userAuthentication, args=(client_connection, client_address,)).start()
+                threading.Thread(target=self.handleRequest, args=(client_connection, client_address,)).start()
         except KeyboardInterrupt as e:
             print("Shutting down Server")
         finally:
             self.secure_server_socket.close()
             self.server_socket.close()
 
-    def handleRequest(self, client_connection, client_address):
+    def handleRequest2(self, client_connection, client_address):
         try:
             file_request = client_connection.recv(MAX_RECEIVE).decode()
             print(f"Message from {client_address}: {file_request}")
@@ -55,18 +53,34 @@ class Server:
         finally:
             client_connection.close()
 
-    def userAuthentication(self, client_connection, client_address):
-        received_login = client_connection.recv(MAX_RECEIVE).decode()
-        username = received_login.split(" ")[0]
-        password = received_login.split(" ")[1]
+    def handleRequest(self, client_connection, client_address):
+        try:
+            request = client_connection.recv(MAX_RECEIVE).decode()
+            print(request)
+            if request.split(" ")[0] == "upload":
+                filename = request.split(" ")[1] + "2"
+                filedata = request.split(" ")[2]
+                self.upload(filename, filedata)
+            elif request.split(" ")[0] == "delete":
+                pass
+            elif request.split(" ")[0] == "download":
+                pass
+            elif request == "see":
+                pass
+        except Exception as e:
+            print(f"{e}")
 
-        if username in self.users and self.users[username] == password:
-            client_connection.send("OK".encode())
-            self.handleRequest(client_connection, client_address)
-        else:
-            client_connection.send("BAD".encode())
-            client_connection.close()
-
+    def upload(self, filename, data=None):
+        try:
+            with open(filename, 'wb') as file:
+                data = data.encode()
+                file.write(data)
+        except FileExistsError as e:
+            print(f"{e}")
+        except KeyboardInterrupt as e:
+            print(f"{e}")
+        except Exception as e:
+            print(f"{e}")
 
 if __name__ == "__main__":
     Server()
